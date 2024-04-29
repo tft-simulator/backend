031cbaa5411b1eac1c9f9c58a227743e07180d33
ned-razzz <smg9972@gmail.com>
2024-04-29 20:11:50 +0900

    project init.

---
+++ src/main/java/dual/project/tftsimulator/TftSimulatorApplication.java
@@ -0,0 +1,13 @@
+package dual.project.tftsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dual/project/tftsimulator/TftSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dual.project.tftsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
